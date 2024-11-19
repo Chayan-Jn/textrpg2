ebde98a1f44770b935e6e86b7b2ef772598a51d1
chayan <chayanj39@gmail.com>
2024-11-19 22:16:37 +0530

    player goes back to start if he dies

--- Screens/BattleScreen.jsx
+++ Screens/BattleScreen.jsx
@@ -2,7 +2,7 @@ import Nav from "../Assets/Nav"
 import '../styles/BattleScreen.css'
 import { getData } from "../Contexts/PlayerHealth";
 import { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 
 //Player Image
 const playerImage = '../images/player/fox1.gif'
@@ -26,6 +26,7 @@ export const BattleScreen = ({images,info,edmg,	eHP,mEHP,next,dieTime}) => {
 	const{message,Name} = info
 	const[attacking,setAttacking] = useState(false)
 	const[clear,setClear] = useState(false)
+	const[playerDead,setPlayerDead] = useState(false) // to reset if player dies
 
 	 
 	
@@ -51,9 +52,23 @@ export const BattleScreen = ({images,info,edmg,	eHP,mEHP,next,dieTime}) => {
 				setLog(playerHP <= 0 ? 'You died' : 'Enemy died');
 				setFightend(true);
 				setTimeout(()=>setClear(true),dieTime*1000)
+				if(playerHP<=0) setPlayerDead(true)
 				
 			}
 		}, [playerHP, enemyHP]);
+
+	
+		// navigate to /beginning after death
+		const navigate = useNavigate(); 
+		useEffect(()=>{
+			setTimeout(()=>{
+				if(playerHP<=0){
+					navigate("/beginning")
+					window.location.reload()
+				}
+			},3000)
+
+		},[playerHP])
 
 
 	const [log,setLog] = useState('');
@@ -62,50 +77,53 @@ export const BattleScreen = ({images,info,edmg,	eHP,mEHP,next,dieTime}) => {
 
 
   return (
-    <>
-      <Nav setEnemyHP={setEnemyHP}/>
-      <div className="battle-page">
+	<>
 
-			<div className="battle-screen">
-				<div className="top">
-					{log==''?message:log}
+    {!playerDead&&<>
+		<Nav setEnemyHP={setEnemyHP}/>
+		<div className="battle-page">
+				<div className="battle-screen">
+					<div className="top">
+						{log==''?message:log}
+					</div>
+					<div className="fight">
+						<div className="player">
+
+							{!attacking&& <img src={playerImage} className="image"/>}
+							{attacking &&
+							<img src={playerAttackImage}className="image"></img>}
+						</div>
+						<div className="enemy">
+							<div className="enemy-nav">
+								<p className="name">{Name}</p>
+								<p className="enemy-hp">HP {enemyHP}/{maxEnemyHP}</p>
+							</div>	
+							{!fightend &&
+							<img src={enemyImage} className="enemy-image"/>}
+
+							{fightend && !clear &&<img src={enemyDeathImage} className="enemy-image"/>}
+						</div>
+					</div>
 				</div>
-				<div className="fight">
-					<div className="player">
 
-						{!attacking&& <img src={playerImage} className="image"/>}
-						{attacking &&
-						<img src={playerAttackImage}className="image"></img>}
-					</div>
-					<div className="enemy">
-						<div className="enemy-nav">
-							<p className="name">{Name}</p>
-							<p className="enemy-hp">HP {enemyHP}/{maxEnemyHP}</p>
-						</div>	
-						{!fightend &&
-						<img src={enemyImage} className="enemy-image"/>}
-
-						{fightend && !clear &&<img src={enemyDeathImage} className="enemy-image"/>}
-					</div>
-				</div>
-			</div>
-
-			{fightend==false?(
-				attacking==true?			
-					<div className="Actions">
-							<div className="atk-btn" onClick={attack} style={{opacity:0.5}}>
-								Attack
-							</div>
-					</div>:			
-					<div className="Actions">
-							<div className="atk-btn" onClick={attack} >
-								Attack
-							</div>
-					</div>
-			)
-				:<Link to={next}><button>Proceed</button></Link> 
-			}
-      </div>
-    </>
+				{fightend==false?(
+					attacking==true?			
+						<div className="Actions">
+								<div className="atk-btn" onClick={attack} style={{opacity:0.5}}>
+									Attack
+								</div>
+						</div>:			
+						<div className="Actions">
+								<div className="atk-btn" onClick={attack} >
+									Attack
+								</div>
+						</div>
+				)
+					:<Link to={next}><button>Proceed</button></Link> 
+				}
+		</div>
+		</> }
+	{playerDead && <h1>You Died. Going Back to Sleep</h1>}
+	</>
   )
 }
